80bdb169e9b6317198ed14e1716bef722d0d7c95
Victor Granic <vmg@boreal321.com>
2017-02-24 10:54:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# bottle - A Slack bot.
+
+All the hard stuff is made easy because of https://github.com/slackapi/python-slackclient
+
+## Installing and running
+
+Add a bot to the org: https://everlong.slack.com/apps/manage/custom-integrations
+Take note of the token. It will be required as an environment variable
+SLACK_BOT_TOKEN
+
+
+```
+mkdir botle
+cd bottle
+virtualenv venv
+source venv/bin/activate
+git clone https://github.com/boreal/bottle.git
+pip install -r requirements.txt
+export SLACK_BOT_ID=xxxx export SLACK_API_TOKEN=xxxx export SLACK_BOT_TOKEN=xxxx; python bottle.py
+```
---
+++ bottle.py
@@ -0,0 +1,304 @@
+import os
+import json
+import socket
+import requests
+import time
+from slackclient import SlackClient
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+
+BOT_ID = os.environ.get("SLACK_BOT_ID")
+BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+API_TOKEN = os.environ.get("SLACK_API_TOKEN")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+
+# instantiate slack client
+slack_client = SlackClient(BOT_TOKEN)
+
+# declare the users, channels and groups objects for caching
+channels = None
+groups = None
+users = None
+
+def get_channels():
+    global channels
+    url = 'https://slack.com/api/channels.list?token=' + API_TOKEN
+    channels_response = http_request(url, None, None, 10, 2)
+    channels = json.loads(channels_response.text)
+    channels['cache_timestamp'] = time.time()
+    channels_response = None
+
+def get_groups():
+    global groups
+    url = 'https://slack.com/api/groups.list?token=' + API_TOKEN
+    groups_response = http_request(url, None, None, 10, 2)
+    groups = json.loads(groups_response.text)
+    groups['cache_timestamp'] = time.time()
+    groups_response = None
+
+def get_users():
+    global users
+    url = 'https://slack.com/api/users.list?token=' + API_TOKEN
+    users_response = http_request(url, None, None, 10, 2)
+    users = json.loads(users_response.text)
+    users['cache_timestamp'] = time.time()
+    users_response = None
+
+'''
+Return the channel object when the channel name or ID is provided. Need to use
+the "channels" endpoint for public channels and the "groups" endpoint for
+private channels.
+'''
+def channel_info(channel):
+    global channels, groups
+
+    # public channels are in channels
+    if (channels is None):
+        print "channels object is empty. Fetching from source."
+        perf_time_before = time.clock()
+        get_channels()
+        perf_time_after = time.clock()
+        perf_time = perf_time_after - perf_time_before
+        print "Fetching channels from source took ", perf_time, "seconds."
+    elif ( ( time.time() - channels['cache_timestamp'] ) > 600 ):
+        print "channels object contents are older than 10 min. Re-fetching from source."
+        get_channels()
+    else:
+        print "channels object is populated and fresh."
+
+    result = None
+    perf_time_before = time.clock()
+    for x in range (0, len(channels['channels'])):
+        channels['channels'][x]['chan_type'] = 'public'
+        if (channels['channels'][x]['name'] == channel):
+            result = channels['channels'][x]
+            break
+        elif (channels['channels'][x]['id'] == channel):
+            result = channels['channels'][x]
+            break
+    perf_time_after = time.clock()
+    perf_time = perf_time_after - perf_time_before
+    print "Searching channels took ", perf_time, "seconds."
+
+    print "channel result: %s" % (result)
+    if result is not None:
+        result['chan_type'] = 'public'
+        return result
+
+    # private channels are in groups
+    if ( groups is None ):
+        print "groups object is empty. Fetching from source."
+        perf_time_before = time.clock()
+        get_groups()
+        perf_time_after = time.clock()
+        perf_time = perf_time_after - perf_time_before
+        print "Fetching groups from source took ", perf_time, "seconds."
+    elif ( ( time.time() - groups['cache_timestamp'] ) > 600 ):
+        print "groups object contents are older than 10 min. Re-fetching from source."
+        get_groups()
+    else:
+        print "groups object is populated and fresh."
+
+    result = None
+    perf_time_before = time.clock()
+    for x in range (0, len(groups['groups'])):
+        groups['groups'][x]['chan_type'] = 'private'
+        if ( groups['groups'][x]['name'] == channel ):
+            result = groups['groups'][x]
+            break
+        elif ( groups['groups'][x]['id'] == channel ):
+            result = groups['groups'][x]
+            break
+    perf_time_after = time.clock()
+    perf_time = perf_time_after - perf_time_before
+    print "Searching groups took ", perf_time, "seconds."
+
+    print "group result: %s" % (result)
+    if result is not None:
+        result['chan_type'] = 'private'
+    return result
+
+'''
+Return the user object when the user name or ID is provided.
+'''
+def user_info(user):
+    global users
+
+    if (users is None):
+        print "users object is empty. Fetching from source."
+        perf_time_before = time.clock()
+        get_users()
+        perf_time_after = time.clock()
+        perf_time = perf_time_after - perf_time_before
+        print "Fetching users from source took ", perf_time, "seconds."
+    elif ( ( time.time() - users['cache_timestamp'] ) > 600 ):
+        print "users object contents are older than 10 min. Re-fetching from source."
+        get_users()
+    else:
+        print "users object is populated and fresh."
+
+    result = None
+    perf_time_before = time.clock()
+    for x in range (0, len(users['members'])):
+        if (users['members'][x]['name'] == user):
+            result = users['members'][x]
+            break
+        elif (users['members'][x]['id'] == user):
+            result = users['members'][x]
+            break
+    perf_time_after = time.clock()
+    perf_time = perf_time_after - perf_time_before
+    print "Searching users took ", perf_time, "seconds."
+
+    print "user result: %s" % (result)
+    return result
+
+def invite_new_hire( params ):
+    response = []
+    welcome_to_league = 'https://everlong.atlassian.net/wiki/pages/viewpage.action?pageId=13506051'
+    #default_channels = ['back-end-team', 'front-end-team', 'product-support', 'product-team', 'production-deployment', 'release_qa', 'tech', 'leaguelife', 'market-news', 'dose-of-wellness', 'api-announcements', 'backend-pull-requests', 'mobile-apps-team', 'product-member-ux', 'beer-o_clock']
+    default_channels = ['onboard-test', 'onboard-test-1', 'onboard-test-2', 'onboard-test-3']
+    user = user_info(params[2])
+    # No matching user found.
+    if user is None:
+        return "User not found"
+    # No channels specified therefore add the user to the default channels.
+    if ( len(params) < 4 ):
+        for channel in default_channels:
+            chaninfo = channel_info(channel)
+            print "CHANNEL INFO: %s" % chaninfo
+            print "USER INFO: %s" % user
+            if ( chaninfo['chan_type'] == 'private' ):
+                url = "https://slack.com/api/groups.invite?token=%s&user=%s&channel=%s" % (API_TOKEN, user['id'], chaninfo['id'])
+            elif ( chaninfo['chan_type'] == 'public' ):
+                url = "https://slack.com/api/channels.invite?token=%s&user=%s&channel=%s" % (API_TOKEN, user['id'], chaninfo['id'])
+            response_json = http_request(url, None, None, 10, 2)
+            print "invitation response: %s" % (response_json.text)
+            try:
+                re = json.loads( response_json.text )
+            except (ValueError, KeyError, TypeError):
+                print "JSON format error: %s" % (response_json.text)
+                return "Something went wrong loading up the JSON response"
+            re['failchan'] = chaninfo['name']
+            response.append(re)
+
+    return response
+
+def http_request(url, data=None, headers=None, timeout=20, tries=1):
+    if headers is None:
+        headers = {}
+    headers['user_agent'] = "Mozilla/5.0 (Bottle 1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+
+    while tries > 0:
+        tries -= 1
+        try:
+            if data is None:
+                response = requests.get(url, headers=headers, timeout=timeout, verify=False)
+            else:
+                response = requests.post(url, data=data, headers=headers, timeout=timeout, verify=False)
+
+        except requests.exceptions.RequestException as error:
+            response = "HTTP Request Exception trying to get %s - Error: %s" % (url, error)
+            time.sleep(1)
+            continue
+
+        if response.status_code == 503:
+            time.sleep(1)
+        else:
+            tries = 0
+
+    return response
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        print output_list
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                #return output['text'].split(AT_BOT)[1].strip().lower(), \
+                return output['text'].split(AT_BOT)[1].strip(), \
+                       output['channel']
+    return None, None
+
+def handle_command(command, channel):
+    # Default message when calling the bot.
+    send_response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+               "* command delimited by spaces. Like so:\n" + \
+               '```@bottle do chan leaguelife\n@bottle do onboard aristotle```'
+    if command.startswith(EXAMPLE_COMMAND):
+        #slack_client.api_call("chat.postMessage", channel=channel, text="Processing your request...", as_user=True)
+        commands = command.split()
+        if (commands[1] == "onboard" ):
+            success = ''
+            fail = ''
+            response = invite_new_hire(commands)
+            if isinstance(response, list):
+                for invite in response:
+                    if ( invite['ok'] ):
+                        print "invite ok: %s" % invite
+                        if 'channel' in invite:
+                            success += invite['channel']['name'] + " "
+                        else:
+                            success += invite['group']['name'] + " "
+                    else:
+                        print "invite bad: %s" % invite
+                        if 'channel' in invite:
+                            fail += invite['failchan'] + " "
+                        else:
+                            fail += invite['failchan'] + " "
+                if success:
+                    send_response = "%s was added to %s." % (commands[2], success)
+                if fail:
+                    send_response += " Failed to add %s to %s." % (commands[2], fail)
+            else:
+                send_response = response
+
+        # Get channel info
+        elif (commands[1] == "chan"):
+            response = channel_info(commands[2])
+            if response is not None:
+                send_response = '```Channel Name: ' + response['name'] + '\n'
+                send_response += 'Channel ID: ' + response['id'] + '\n'
+                if ( response['chan_type'] == 'public' ):
+                    send_response += 'Member Count: %d\n' % (response['num_members'])
+                send_response += 'Channel type: ' + response['chan_type'] + '```'
+            else:
+                send_response = "Invalid %s parameter: %s" % (commands[1], commands[2])
+        # Get user info
+        elif (commands[1] == "user"):
+            response = user_info(commands[2])
+            if response is not None:
+                send_response = '```User Name: ' + response['name'] + '\n'
+                send_response += 'User ID: ' + response['id'] + '\n'
+                send_response += 'Real Name: ' + response['profile']['first_name'] + " " + response['profile']['last_name'] + '```'
+            else:
+                send_response = "Invalid %s parameter: %s" % (commands[1], commands[2])
+        # No command given. Send snarky reply.
+        else:
+            send_response = "Sure...write some more code then I can do that!"
+
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=send_response, as_user=True)
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("bottle connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
